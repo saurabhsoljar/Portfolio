5d8878f9b314ee7a6bc870ba5f5be8abb2c1126a
saurabh kumar <93548806+saurabhsoljar@users.noreply.github.com>
2023-08-20 12:45:37 +0530

    Add Testimonials Section

--- script.js
+++ script.js
@@ -64,17 +64,25 @@ $('.gallery').magnificPopup({
 //Testimonial Section Starts
 $('.testimonials-container').owlCarousel({
     loop:true,
+    autoplay:true,
+    autoplayTime:2000,
     margin:10,
     nav:true,
+    navText: [
+        "<i class='fa-solid fa-arrow-left'></i>",
+        "<i class='fa-solid fa-arrow-right'></i>"
+    ],
     responsive:{
         0:{
-            items:1
+            items:1,
+            nav:false
         },
         600:{
-            items:3
+            items:1,
+            nav:true
         },
-        1000:{
-            items:5
+        768:{
+            items:2
         }
     }
 })
